81dfa8a98fa7b24167c5014cc59462a5ae02b9cc
dingyi <dingyi@roborock.com>
2021-12-10 19:53:10 +0800

    add PriorityFixedQueue

---
+++ PriorityFixedQueue.h
@@ -0,0 +1,27 @@
+#include <queue>
+
+template <typename DataType>
+class PriorityFixedQueue {
+public:
+  PriorityFixedQueue(int cap) : _cap(cap) {}
+  void Push(DataType value) {
+    if (static_cast<int>(_q.size()) < _cap)
+      _q.push(value);
+    else if (value > _q.top()) {
+      _q.pop();
+      _q.push(value);
+    }
+  }
+
+  void GetItems(std::vector<DataType>& data_arr) {
+    data_arr.resize(_q.size());
+    while (_q.size()) {
+      data_arr[_q.size() - 1] = _q.top();
+      _q.pop();
+    }
+  }
+
+private:
+  int _cap;
+  std::priority_queue<DataType, std::vector<DataType>, std::greater<DataType>> _q;
+};
---
+++ test_PriorityFixedQueue.cpp
@@ -0,0 +1,21 @@
+#include "PriorityFixedQueue.h"
+#include <iostream>
+
+int main()
+{
+  PriorityFixedQueue<int> queue(3);
+  queue.Push(3);
+  queue.Push(4);
+  queue.Push(7);
+  queue.Push(2);
+  queue.Push(5);
+
+  std::vector<int> data_arr;
+  queue.GetItems(data_arr);
+  for(auto data: data_arr) {
+    std::cout << data << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
