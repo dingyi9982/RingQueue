db5db1624fd19f08a373a1d15fe42b2161cd7508
dingyi <dingyi@roborock.com>
2021-12-13 17:23:25 +0800

    fix bug

--- PriorityFixedQueue.h
+++ PriorityFixedQueue.h
@@ -1,12 +1,11 @@
 #include <queue>
+#include <mutex>
 
-template <typename DataType>
-class DescendingFixedQueue {
+template <typename DataType> class DescendingFixedQueue {
 public:
-  DescendingFixedQueue(int cap)
-    : _cap(cap) {}
-  void Push(DataType value)
-  {
+  DescendingFixedQueue(int cap) : _cap(cap) {}
+  void Push(DataType value) {
+    std::lock_guard<std::mutex> lock(_mutex);
     if (static_cast<int>(_q.size()) < _cap)
       _q.push(value);
     else if (value > _q.top()) {
@@ -15,8 +14,8 @@ public:
     }
   }
 
-  void PopAll(std::vector<DataType>& data_arr)
-  {
+  void PopAll(std::vector<DataType>& data_arr) {
+    std::lock_guard<std::mutex> lock(_mutex);
     data_arr.resize(_q.size());
     while (_q.size()) {
       data_arr[_q.size() - 1] = _q.top();
@@ -26,16 +25,15 @@ public:
 
 private:
   int _cap;
+  std::mutex _mutex;
   std::priority_queue<DataType, std::vector<DataType>, std::greater<DataType>> _q;
 };
 
-template <typename DataType>
-class AscendingFixedQueue {
+template <typename DataType> class AscendingFixedQueue {
 public:
-  AscendingFixedQueue(int cap)
-    : _cap(cap) {}
-  void Push(DataType value)
-  {
+  AscendingFixedQueue(int cap) : _cap(cap) {}
+  void Push(DataType value) {
+    std::lock_guard<std::mutex> lock(_mutex);
     if (static_cast<int>(_q.size()) < _cap)
       _q.push(value);
     else if (value < _q.top()) {
@@ -44,8 +42,8 @@ public:
     }
   }
 
-  void PopAll(std::vector<DataType>& data_arr)
-  {
+  void PopAll(std::vector<DataType>& data_arr) {
+    std::lock_guard<std::mutex> lock(_mutex);
     data_arr.resize(_q.size());
     while (_q.size()) {
       data_arr[_q.size() - 1] = _q.top();
@@ -55,5 +53,6 @@ public:
 
 private:
   int _cap;
+  std::mutex _mutex;
   std::priority_queue<DataType, std::vector<DataType>, std::less<DataType>> _q;
 };
